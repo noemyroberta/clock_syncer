52c9806704ef524f5c7d0ed810e33aa8be1f351d
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-03-02 21:51:45 -0300

    feat: add calls for server on client

--- client.py
+++ client.py
@@ -10,5 +10,11 @@ def run():
     response = stub.Sync(clock.SyncRequest(client_time=int(time.time())))
     print("Server time:", response.server_time)
 
+    time_before_adjustment = int(time.time())
+    offset = response.server_time - time_before_adjustment
+    adjusted_time = time_before_adjustment + offset
+    
+    print("Adjusted client time:", adjusted_time)
+
 if __name__ == '__main__':
     run()
